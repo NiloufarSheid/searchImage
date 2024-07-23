7e3b42e224f2e06b46b57a906fe30d8a14bc2c7f
NiloufarSheyd <155656698+NiloufarSheid@users.noreply.github.com>
2024-07-23 13:49:40 +0330

    starting project with html& css

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const App = () => {
+  return (
+    <>
+    <div className='header'>
+      <span>جستجو</span>
+      <input type="text" />
+      <button>ارسال</button>
+    </div>
+    <div className="gallery">
+      <img src="" alt="" />
+    </div>
+    </>
+  );
+}
+
+export default App;
