937180afa82e4648855f77825b270136ad8bd712
NiloufarSheyd <155656698+NiloufarSheid@users.noreply.github.com>
2024-07-23 14:55:48 +0330

    get data from input using usestate

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const App = () => {
+  const [data,setData]=useState("");
+  const getData=(e)=>{
+    setData(e.target.value)
+  }
+ 
+    console.log(data)
+
+
   return (
     <>
     <div className='header'>
       <span>جستجو</span>
-      <input type="text" />
+      <input onChange={getData} value={data} type="text" />
       <button>ارسال</button>
     </div>
     <div className="gallery">
