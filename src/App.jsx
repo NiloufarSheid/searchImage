8f2446a7c1de7a0c63b14a6a438717034c51222d
NiloufarSheyd <155656698+NiloufarSheid@users.noreply.github.com>
2024-07-23 18:21:12 +0330

    set function for send button

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,16 @@ import React, { useState } from 'react';
 
 const App = () => {
   const [data,setData]=useState("");
-  //FdldoVW5XaneXYyrGzrVfFvJTaKcHHs2hlAryb5q2po
   const getData=(e)=>{
     setData(e.target.value)
   }
  const fetchImage=()=>{
-  fetch("https://api.unsplash.com/search/photos/?client_id=FdldoVW5XaneXYyrGzrVfFvJTaKcHHs2hlAryb5q2po")
+  fetch(`https://api.unsplash.com/search/photos/?client_id=FdldoVW5XaneXYyrGzrVfFvJTaKcHHs2hlAryb5q2po&query=${data}`)
+  .then(res => res.json())
+  .then(data => {
+    console.log(data.results)
+
+  })
  }
 
 
@@ -16,7 +20,7 @@ const App = () => {
     <div className='header'>
       <span>جستجو</span>
       <input onChange={getData} value={data} type="text" />
-      <button>ارسال</button>
+      <button onClick={fetchImage}>ارسال</button>
     </div>
     <div className="gallery">
       <img src="" alt="" />
